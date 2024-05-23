213b27a6b39a5e6f48f0c2676ffd208d7025bc3f
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-05-23 12:41:14 +0100

    clean baseController

--- src/controllers/base.ts
+++ src/controllers/base.ts
@@ -1,12 +1,7 @@
-import { Request, Response } from "express";
 import Room from "../models/Room";
-import { getRoomRepository } from "../repositories/Room";
-import RoomRepository from "../repositories/Room/repository";
 import { TALK_HOST, TALK_PORT } from "../repositories/env";
 
 class BaseController {
-    room_rep: RoomRepository = getRoomRepository("good");
-
 
     /**
     * get a room link
