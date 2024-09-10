72102fd43c284b89e0bab6896ae37444a44b8d20
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-09-10 09:52:39 +0100

    update env variable and its documentations

--- README-container.md
+++ README-container.md
@@ -14,11 +14,23 @@ docker run --env-file ./example.env opencare-patient-backend [--network <network
 
 ## Environment Variables
 
-- `MONGO_HOST` - mongo host 📌
-- `MONGO_PORT` - mongo port (27017)
-- `MONGO_USER` - mongo user used (define for authentification)
-- `MONGO_PASSWORD` - password of mongo user (define for authentification)
-- `FRONTEND_HOST` - host of the frontend 📌
-- `FRONTEND_PORT` - port of the frontend (80)
-- `SERVER_PORT` - port of this backend (3001)
-- `KEY_TOKEN` - key token 📌
+* `O3_URL` - o3 url 📌
+* `O3_USER` - o3 user 📌
+* `O3_PASSWORD` - o3 password 📌
+* `MONGO_HOST` - mongo url for connection 📌
+* `BASE_PASSWORD_PATIENT` - base password of a patient (123456)
+* `ODOO_HOST` - odoo url 📌
+* `ODOO_PORT` - odoo url 📌
+* `ODOO_DB` - odoo db 📌
+* `ODOO_USER` - odoo user 📌
+* `ODOO_PASSWORD` - odoo password 📌
+* `ODOO_CODE_SERVICE` - base de code du service (OPENCARES)
+* `ODOO_PRICE_SERVICE` - prix de base d'un service (1500)
+* `TALK_URL` - talk url 📌
+* `TALK_USER` - talk user 📌
+* `TALK_PASSWORD` - talk password 📌
+* `TALK_INIT_PASSWORD` - base password of patient and doctor on talk (TALK_PASSWORD)
+* `SERVER_PORT` - port of this backend (3001)
+* `KEY_TOKEN` - key token 📌
+* `ODOO_BANK_ID` - bank id for odoo (6)
+* `ODOO_CASH_ID` - cash id for odoo (7)
--- src/repositories/env.ts
+++ src/repositories/env.ts
@@ -1,30 +1,22 @@
-import { SECURE } from "../config";
 import { base64 } from "../utils";
 
 // O3 env variables
 const O3_USER = process.env.O3_USER;
 const O3_PASSWORD = process.env.O3_PASSWORD;
-const O3_PORT = process.env.O3_PORT;
-const O3_HOST = process.env.O3_HOST;
+const O3_URL = process.env.O3_URL;
 
-export const BASE_SECURE = `http${SECURE ? 's' : ''}`;
-
-export const O3_BASE_URL = `http://${O3_HOST}${O3_PORT ? `:${O3_PORT}` : ""}/openmrs/ws/rest/v1`;
-export const O3_BASEF_URL = `http://${O3_HOST}${O3_PORT ? `:${O3_PORT}` : ""}/openmrs/ws/fhir2/R4`;
+export const O3_BASE_URL = `${O3_URL}/openmrs/ws/rest/v1`;
+export const O3_BASEF_URL = `${O3_URL}/openmrs/ws/fhir2/R4`;
 export const O3_BASE64 = base64(`${O3_USER}:${O3_PASSWORD}`);
 
 // Talk env variables
-export const TALK_USER = process.env.TALK_USER;
-export const TALK_PASSWORD = process.env.TALK_PASSWORD;
-export const TALK_PORT = process.env.TALK_PORT;
-export const TALK_HOST = process.env.TALK_HOST;
+const TALK_USER = process.env.TALK_USER;
+const TALK_PASSWORD = process.env.TALK_PASSWORD;
+const TALK_URL = process.env.TALK_URL;
 
 export const TALK_BASE64 = base64(`${TALK_USER}:${TALK_PASSWORD}`);
-
-export const TALK_URL = `${BASE_SECURE}://${TALK_HOST}${TALK_PORT ? `:${TALK_PORT}` : ""}`
-export const NC_BASE_URL = `${BASE_SECURE}://${TALK_HOST}${TALK_PORT ? `:${TALK_PORT}` : ""}/ocs/v2.php`
+export const NC_BASE_URL = `${TALK_URL}/ocs/v2.php`
 export const TALK_BASE_URL = `${NC_BASE_URL}/apps/spreed/api/v4`;
-
 export const TALK_BASE_PASSWORD = process.env.TALK_INIT_PASSWORD;
 
 //Odoo env variables
